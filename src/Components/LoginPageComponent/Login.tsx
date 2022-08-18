e01f5c385788a3901f61fd2492d6d7f840264fff
Cycatz L1 <piyushgohan@gmail.com>
2022-08-18 15:08:21 +0530

    new files added

--- src/Components/LoginPageComponent/Login.tsx
+++ src/Components/LoginPageComponent/Login.tsx
@@ -1,23 +1,78 @@
-import { Typography } from "@mui/material"
-import bannerImg from '../../Assets/LoginPage/Vector_2677 2.svg';
-import LazyImage from "../../Utils/LazyImage";
-import React from "react";
-import {Link} from 'react-router-dom';
+import Box from '@mui/material/Box';
+import {Formik , Form , Field} from 'formik';
+import {Button, TextField } from '@mui/material';
+import * as Yup from 'yup';
+import logo from '../../Assets/LoginPage/imageLogo.svg';
+import LazyImage from '../../Utils/LazyImage';
+import LoginIcon from '@mui/icons-material/Login';
+import style from './Login.module.css';
 
-const Login = () => {
-  return (
-    <React.Fragment>
-        <div>
-            <LazyImage src={bannerImg} alt="banner-img" width="100%" />
-            <Typography variant="h2">
-                Enjoy the <span>Cyber Security</span> Experience
-            </Typography>
-            <div>
-                <button></button>
-            </div>
-        </div>
-    </React.Fragment>
-  )
+type Props = {
+    forgotClick : () => any,
+}
+
+
+const Login = (props : Props) => {
+
+
+    const initialValues = {
+        email : "",
+        password : "",
+    }
+
+    const validationSchema = Yup.object({
+        email: Yup.string().email("Invalid Email Address").required("Required"),
+        password : Yup.string()
+        .required('No password provided.') 
+        .min(8, 'Password is too short - should be 8 chars minimum.')
+        .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
+    })
+
+    function handleSubmit(){
+
+    
+    }
+
+    return (
+
+        <Box
+            component="div"
+            sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+            }}
+            className={style.formbox}
+        >
+            <LazyImage src={logo} width="150" height="80" alt="logo" />
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={() => handleSubmit()}
+            >
+                {({ setFieldValue, values }) => (
+                    <Form className={style.loginForm}>
+                        <Field
+                            component={TextField}
+                            name="email"
+                            label="Email Address"
+                            variant="outlined"
+                            fullWidth
+                        />
+                        <Field
+                            component={TextField}
+                            name="password"
+                            label="Password"
+                            variant="outlined"
+                            fullWidth
+                        />
+                        <p>Forgot Password ?</p>
+                        <Button variant="contained" endIcon={<LoginIcon />} fullWidth>Sign In</Button>
+                    </Form>
+                    
+                )}
+            </Formik>
+
+        </Box>
+    )
 }
 
 export default Login
--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,39 @@
-import Login from "../../Components/LoginPageComponent/Login"
+import { Typography } from "@mui/material"
+import bannerImg from '../../Assets/LoginPage/Vector_2677 2.svg';
+import LazyImage from "../../Utils/LazyImage";
+import {Link} from 'react-router-dom';
+import CircleIcon from '@mui/icons-material/Circle';
+import {useState} from 'react';
+import style from './LoginPage.module.css';
 
-const LoginPage = () => {
+
+import Login from "../../Components/LoginPageComponent/Login";
+
+const LoginPage:React.FC = () => {
+
+  const [forgotClick , setForgotClicked] = useState<boolean>(false);
+
+  function handleForgot(){
+
+  }
   return (
-    <div>
-        <Login />
-    </div>
+        <div className={style.bannerContainer}>
+          <div>
+              <LazyImage src={bannerImg} alt="banner-img" width="100%" />
+              <Typography variant="h3" className={style.bannerHeading}>
+                  Enjoy the <span className={style.spanHeading}>Cyber Security</span> <br/> Experience
+              </Typography>
+              <div className={style.buttonFlex}>
+                  <Link to="/" className={style.buttons}><CircleIcon style={{fontSize : "10px" , marginRight : "15px"}} /> Attack Surface</Link>
+                  <Link to="/" className={style.buttons}><CircleIcon style={{fontSize : "10px" , marginRight : "15px"}} />Breach Notifcation</Link>
+                  <Link to="/" className={style.buttons}><CircleIcon style={{fontSize : "10px" , marginRight : "15px"}} />DarkWeb Monitoring</Link>
+                  <Link to="/" className={style.buttons}><CircleIcon style={{fontSize : "10px" , marginRight : "15px"}} />Brand Monitoring</Link>
+                  <Link to="/" className={style.buttons}><CircleIcon style={{fontSize : "10px" , marginRight : "15px"}} />Email Security</Link>
+                  <Link to="/" className={style.buttons}><CircleIcon style={{fontSize : "10px" , marginRight : "15px"}} />Mobile Security</Link>
+              </div>
+              <Login forgotClick={() => handleForgot()}/>
+          </div>
+      </div>
   )
 }
 
